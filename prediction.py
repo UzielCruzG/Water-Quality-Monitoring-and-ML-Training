b3c3c4a3347aa1b0bfb02c81716aad0d04419f12
Güily Uziel Cruz Gallo <willyuzielinazuma@gmail.com>
2024-05-06 10:58:47 -0600

    Prediction update

--- prediction.py
+++ prediction.py
@@ -2,14 +2,12 @@ import joblib
 import numpy as np
 
 # Cargar el modelo desde el archivo
-loaded_model = joblib.load('modelo_entrenado.joblib')
+loaded_model = joblib.load('models/modelo_entrenado.joblib')
 
 # Ejemplo de dato a predecir
-dato = np.array([[0.8215488215488218,0.32244977443120965,0.4427792915531335,0.23529411764705876]]) #Cambiar a que python lea los valores en tiempo real
+dato = np.array([[0.8215488215488218,0.32244977443120965,0.4427792915531335,0.23529411764705876]])
 
 # Realizar predicciones con el modelo cargado
 y_pred_loaded = loaded_model.predict(dato)
 
 print("Pertence a la clase ",y_pred_loaded)
-
-#Recuerda cambiar la entrada d evalores, normalizar los datos en tiempo real.
