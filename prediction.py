ec00636a7c7226427efbaaee21781f0d6e8a2ca0
Güily Uziel Cruz Gallo <willyuzielinazuma@gmail.com>
2024-05-09 13:04:34 -0600

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,41 +2,95 @@ import joblib
 import numpy as np
 import serial
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import time
+#import tensorflow as tf
 
 # Cargar el modelo desde el archivo
-loaded_model = joblib.load('models/modelo_entrenado_Adam_TensorFlow.h5')
+loaded_model = joblib.load('models/modelo_entrenado_MLP.joblib')
+#loaded_model = tf.keras.models.load_model('models/modelo_entrenado_Adam_TensorFlow.h5')
 
 # Inicializar la conexión serial con el ESP32
 ser = serial.Serial('COM5', 115200)
 
-# Inicializar el MinMaxScaler para normalizar los datos
-scaler = MinMaxScaler()
+# Definir los valores máximos y mínimos para normalizar los datos
+max_values = np.array([28.5, 327.12, 367, 11.68])
+min_values = np.array([22.56, 19.01, 0, 4.71])
 
 # Esperar un tiempo para que la conexión serial se estabilice
-time.sleep(2)
+time.sleep(1)
 
 while True:
     # Leer los datos del ESP32
     data = ser.readline().decode().strip()
+    print(data)
 
     # Si hay datos válidos
-    if data:
+    if data and ',' in data:
+        # Separar los valores y convertirlos en un array de numpy
+        values = np.array(data.split(','), dtype=float)
+
+        # Normalizar manualmente los datos
+        values_normalized = (values - min_values) / (max_values - min_values)
+        print(values_normalized)
+
+        # Realizar la predicción con el modelo cargado
+        y_pred_loaded = loaded_model.predict(values_normalized.reshape(1, -1))
+
+        # Imprimir la predicción
+        print("Pertenece a la clase:", y_pred_loaded)
+
+'''
+import joblib
+import numpy as np
+import serial
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import time
+#import tensorflow as tf
+
+# Cargar el modelo desde el archivo
+loaded_model = joblib.load('models/modelo_entrenado_MLP.joblib')
+#loaded_model = tf.keras.models.load_model('models/modelo_entrenado_Adam_TensorFlow.h5')
+
+# Inicializar la conexión serial con el ESP32
+ser = serial.Serial('COM5', 115200)
+
+# Definir los valores máximos y mínimos para normalizar los datos
+max_values = np.array([28.5, 327.12, 367, 11.68])
+min_values = np.array([22.56, 19.01, 0, 4.71])
+
+# Concatenar los valores máximos y mínimos en un solo array
+feature_range = np.vstack((min_values, max_values))
+
+# Inicializar el MinMaxScaler con los valores máximos y mínimos y ajustarlo
+scaler = MinMaxScaler(feature_range=(min_values.min(), max_values.max()))
+scaler.fit(feature_range)
+
+# Esperar un tiempo para que la conexión serial se estabilice
+time.sleep(3)
+
+while True:
+    # Leer los datos del ESP32
+    data = ser.readline().decode().strip()
+    print(data)
+
+    # Si hay datos válidos
+    if data and ',' in data:
         # Separar los valores y convertirlos en un array de numpy
         values = np.array(data.split(','), dtype=float).reshape(1, -1)
 
-        # Crear un DataFrame temporal para aplicar la normalización
-        df_temp = pd.DataFrame(values, columns=['Temperatura', 'TDS', 'Turbidez', 'PH'])
-
-        # Aplicar la normalización a los datos
-        values_normalized = scaler.fit_transform(df_temp)
+        # Normalizar los datos usando el MinMaxScaler ajustado
+        values_normalized = scaler.transform(values)
+        print(values_normalized)
 
         # Realizar la predicción con el modelo cargado
         y_pred_loaded = loaded_model.predict(values_normalized)
 
         # Imprimir la predicción
         print("Pertenece a la clase:", y_pred_loaded)
+
+'''
+
 
 # # Comunicación por bluetooth
 # import bluetooth
@@ -73,19 +127,3 @@ while True:
 
 # # Cerrar el socket
 # sock.close()
-
-'''
-import joblib
-import numpy as np
-
-# Cargar el modelo desde el archivo
-loaded_model = joblib.load('models/modelo_entrenado_Adam_TensorFlow.h5')
-
-# Ejemplo de dato a predecir
-dato = np.array([[0.8215488215488218,0.32244977443120965,0.4427792915531335,0.23529411764705876]])
-
-# Realizar predicciones con el modelo cargado
-y_pred_loaded = loaded_model.predict(dato)
-
-print("Pertence a la clase ",y_pred_loaded)
-'''
